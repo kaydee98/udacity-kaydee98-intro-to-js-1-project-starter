4e8d3b850b9347e08d7cdd24d11ed85d5b370e53
kaydee98 <kay_dee98@hotmail.com>
2024-10-17 10:57:24 -0400

    created the increseQuantity function

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -82,11 +82,21 @@ function addProductToCart(productId){
   - increaseQuantity should then increase the product's quantity
 */
 
+function increaseQuantity(productId){
+
+  for( let i = 0; i < cart.length; i++){
+    if( cart[i].productId == productId){
+      cart[i].quantity += 1;
+      break;
+    }
+  }
+}
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
