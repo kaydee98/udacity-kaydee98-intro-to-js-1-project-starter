b0c0cef7935b078920028738bc5d6402f30cecef
kaydee98 <kay_dee98@hotmail.com>
2024-10-18 10:59:28 -0400

    fixed style issues/errors raised by eslint

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -4,27 +4,27 @@ let totalPaid = 0;
 
 // Define product objects and add them to the products array
 const product1 = {
-  name: "Cherry",
+  name: 'Cherry',
   price: 3.99,
   quantity: 0,
   productId: 101,
-  image: "/images/cherry.jpg"
+  image: '/images/cherry.jpg',
 };
 
 const product2 = {
-  name: "Orange",
+  name: 'Orange',
   price: 2.50,
   quantity: 0,
   productId: 102,
-  image: "/images/orange.jpg"
+  image: '/images/orange.jpg',
 };
 
 const product3 = {
-  name: "Strawberry",
+  name: 'Strawberry',
   price: 4.99,
   quantity: 0,
   productId: 103,
-  image: "/images/strawberry.jpg"
+  image: '/images/strawberry.jpg',
 };
 
 products.push(product1, product2, product3);
@@ -33,94 +33,79 @@ products.push(product1, product2, product3);
 const cart = [];
 
 // Helper function to find the index of a product in the cart by productId
-function findCartProductIndex(productId){
-  return cart.findIndex( p => p.productId === productId);
+function findCartProductIndex(productId) {
+  return cart.findIndex((p) => p.productId === productId);
 }
 
-// Adds a product to the cart by productId. Increases quantity if the product is already in the cart.
+// Adds a product to the cart by productId.
+// Increases quantity if the product is already in the cart.
 function addProductToCart(productId) {
-  let product, cartIndex;
-
   // Get product based on productId from the products array
-  product = products.find( p => p.productId === productId);
-  
-  // Check if the product is in the cart
-  cartIndex = findCartProductIndex(productId);
+  const product = products.find((p) => p.productId === productId);
 
-  if( cartIndex === -1) {
-    product.quantity = 1;  
+  // Check if the product is in the cart
+  const cartIndex = findCartProductIndex(productId);
+
+  if (cartIndex === -1) {
+    product.quantity = 1;
     cart.push(product);
-  }
-  else {
-    cart[cartIndex].quantity += 1
+  } else {
+    cart[cartIndex].quantity += 1;
   }
 }
 
 // Increases the quantity of a product in the cart by productId
-function increaseQuantity(productId){
-  let cartIndex; 
-
-  cartIndex = findCartProductIndex(productId);
+function increaseQuantity(productId) {
+  const cartIndex = findCartProductIndex(productId);
   cart[cartIndex].quantity += 1;
-
 }
 
 // Decreases the quantity of a product in the cart by productId.
 // Removes the product if quantity reaches 0.
-function decreaseQuantity(productId){
-  let cartIndex; 
-
-  cartIndex = findCartProductIndex(productId);
+function decreaseQuantity(productId) {
+  const cartIndex = findCartProductIndex(productId);
   cart[cartIndex].quantity -= 1;
 
   // If the quantity is reduced to 0, remove the product from the cart
-  if (cart[cartIndex].quantity == 0) {
+  if (cart[cartIndex].quantity === 0) {
     cart.splice(cartIndex, 1);
   }
-
 }
 
 // Removes a product from the cart entirely by productId and sets its quantity to 0.
-function removeProductFromCart(productId){
-
-  let cartIndex; 
-
-  cartIndex = findCartProductIndex(productId);
+function removeProductFromCart(productId) {
+  const cartIndex = findCartProductIndex(productId);
   cart[cartIndex].quantity = 0;
-  cart.splice(cartIndex,1);
-
+  cart.splice(cartIndex, 1);
 }
 
-//Calculates the total cost of all products in the cart by summing their price and quantity.
-function cartTotal(){
-
+// Calculates the total cost of all products in the cart by summing their price and quantity.
+function cartTotal() {
   let totalCost = 0;
 
-  for( let i = 0; i < cart.length; i++){
-    let cost = cart[i].quantity * cart[i].price;
+  for (let i = 0; i < cart.length; i += 1) {
+    const cost = cart[i].quantity * cart[i].price;
     totalCost += cost;
-
   }
 
   return totalCost;
-
 }
 
 /* Empties the products from the cart */
-function emptyCart(){
+function emptyCart() {
   cart.splice(0, cart.length);
   totalPaid = 0;
 }
 
-// Processes payment by subtracting the amount paid from the cart total. 
+// Processes payment by subtracting the amount paid from the cart total.
 // Returns the remaining balance. Positive if overpaid, negative if underpaid
 function pay(amount) {
   totalPaid += amount;
-  
-  let diff = totalPaid - cartTotal(); 
+
+  const diff = totalPaid - cartTotal();
   remainingBalance = cartTotal() - totalPaid;
 
-  //reset totalPaid if enough to cover cart total
+  // reset totalPaid if enough to cover cart total
   if (diff >= 0) {
     totalPaid = 0;
   }
@@ -129,15 +114,15 @@ function pay(amount) {
 }
 
 module.exports = {
-   products,
-   cart,
-   addProductToCart,
-   increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
-   /* Uncomment the following line if completing the currency converter bonus */
-   // currency
-}
+  products,
+  cart,
+  addProductToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeProductFromCart,
+  cartTotal,
+  pay,
+  emptyCart,
+  /* Uncomment the following line if completing the currency converter bonus */
+  // currency
+};
